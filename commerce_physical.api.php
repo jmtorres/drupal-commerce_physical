3a865263a77864ea01b14976213a83a70db5668b
rszrama <ryan.szrama@gmail.com>
2011-09-27 19:47:12 -0400

    Add support and documentation for determining the field name to use to determine the phone number from a customer profile; fixed some missing _alter suffixes on documented hook names.

--- commerce_physical.api.php
+++ commerce_physical.api.php
@@ -37,7 +37,7 @@ function hook_commerce_physical_entity_weight_field_name_alter(&$field_name, $en
  *
  * @see commerce_physical_product_line_item_weight()
  */
-function hook_commerce_physical_product_line_item_weight(&$weight, $line_item) {
+function hook_commerce_physical_product_line_item_weight_alter(&$weight, $line_item) {
   // Add a one pound tare weight to the product for shipping calculation.
   $weight['weight'] += physical_weight_convert(array('weight' => 1, 'unit' => 'lb'), $weight['unit']);
 }
@@ -54,7 +54,7 @@ function hook_commerce_physical_product_line_item_weight(&$weight, $line_item) {
  *
  * @see commerce_physical_order_weight()
  */
-function hook_commerce_physical_order_weight(&$weight, $order, $unit) {
+function hook_commerce_physical_order_weight_alter(&$weight, $order, $unit) {
   // No example.
 }
 
@@ -70,7 +70,7 @@ function hook_commerce_physical_order_weight(&$weight, $order, $unit) {
  *
  * @see commerce_physical_line_item_shippable()
  */
-function hook_commerce_physical_line_item_shippable(&$shippable, $line_item) {
+function hook_commerce_physical_line_item_shippable_alter(&$shippable, $line_item) {
   // No example.
 }
 
@@ -86,7 +86,7 @@ function hook_commerce_physical_line_item_shippable(&$shippable, $line_item) {
  *
  * @see commerce_physical_order_shippable()
  */
-function hook_commerce_physical_order_shippable(&$shippable, $order) {
+function hook_commerce_physical_order_shippable_alter(&$shippable, $order) {
   // No example.
 }
 
@@ -104,6 +104,22 @@ function hook_commerce_physical_order_shippable(&$shippable, $order) {
  *
  * @see commerce_physical_order_shipping_field_name()
  */
-function hook_commerce_physical_order_shipping_field_name(&$field_name, $order) {
+function hook_commerce_physical_order_shipping_field_name_alter(&$field_name, $order) {
+  // No example.
+}
+
+/**
+ * Allows modules to specify a different phone number field name for the given
+ * customer profile than the one determined by Commerce Physical Product.
+ *
+ * @param &$field_name
+ *   The name of the field instance on the customer profile that is currently
+ *   specified as the field to use for the customer's phone number.
+ * @param $profile
+ *   The customer profile whose phone number field is being determined.
+ *
+ * @see commerce_physical_profile_phone_number_field_name()
+ */
+function hook_commerce_physical_profile_phone_number_field_name_alter(&$field_name, $profile) {
   // No example.
 }
