e2a88669cc427dbd82339600f5b12e321e2b50dd
chriscalip <chriscalip@210499.no-reply.drupal.org>
2011-10-22 23:29:57 -0400

    Issue #1295224 by chriscalip, rszrama: add hook documentation for the alter hooks pertaining to dimensions and volume calculation.

--- commerce_physical.api.php
+++ commerce_physical.api.php
@@ -33,7 +33,7 @@ function hook_commerce_physical_entity_weight_field_name_alter(&$field_name, $en
  *   item, which defaults to the weight of the product times the quantity.
  * @param $line_item
  *   The product line item object whose weight is being determined. The product
- *   data can be referenced through this line item's commerce_product field.
+ *   data can be accessed through this line item's commerce_product field.
  *
  * @see commerce_physical_product_line_item_weight()
  */
@@ -46,7 +46,8 @@ function hook_commerce_physical_product_line_item_weight_alter(&$weight, $line_i
  * Allows modules to alter the weight that has been determined for an order.
  *
  * @param &$weight
- *   The weight field value array representing the weight of the product.
+ *   The weight field value array representing the weight of the order or NULL
+ *   if no weight has yet been determined.
  * @param $order
  *   The order object whose weight is being determined.
  * @param $unit
@@ -55,6 +56,61 @@ function hook_commerce_physical_product_line_item_weight_alter(&$weight, $line_i
  * @see commerce_physical_order_weight()
  */
 function hook_commerce_physical_order_weight_alter(&$weight, $order, $unit) {
+  // No example.
+}
+
+/**
+ * Allows modules to specify a different dimensions field name for the given
+ * entity than the one determined by Commerce Physical Product.
+ *
+ * @param &$field_name
+ *   The name of the field instance on the entity that is currently specified as
+ *   the dimensions field to use in dimensions and volume calculations.
+ * @param $entity_type
+ *   The type of entity the field is attached to.
+ * @param $entity
+ *   The actual entity whose dimensions value is being determined.
+ *
+ * @see commerce_physical_entity_dimensions_field_name()
+ */
+function hook_commerce_physical_entity_dimensions_field_name_alter(&$field_name, $entity_type, $entity) {
+  // No example.
+}
+
+/**
+ * Allows modules to alter the dimensions that have been determined for a single
+ * product referenced by a product line item.
+ *
+ * @param &$dimensions
+ *   The dimensions field value array representing the length, width, and height
+ *   of a single product referenced by a product line item. These dimensions may
+ *   subsequently be used in volume calculations for an order.
+ * @param $line_item
+ *   The product line item object whose product dimensions are being determined.
+ *   The product data can be accessed through this line item's commerce_product
+ *   field.
+ *
+ * @see commerce_physical_product_line_item_dimensions()
+ */
+function hook_commerce_physical_product_line_item_dimensions_alter(&$dimensions, $line_item) {
+  // No example.
+}
+
+/**
+ * Allows modules to alter the volume that has been determined for an order.
+ *
+ * @param &$volume
+ *   A volume value array with keys representing the total 'volume' of the order
+ *   in the 'unit' specified or NULL if no volume has yet been determined.
+ * @param $order
+ *   The order object whose volume is being calculated.
+ * @param $unit
+ *   The unit of measurement whose corresponding cubic unit will be used for the
+ *   returned volume of the order.
+ *
+ * @see commerce_physical_order_volume()
+ */
+function hook_commerce_physical_order_volume_alter(&$volume, $order, $unit) {
   // No example.
 }
 
